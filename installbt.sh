dc861e95f888803543cb42ef44183fa5f48ada91
olmtv <31463363+olmtv@users.noreply.github.com>
2018-12-28 17:41:01 +0800

    Update installbt.sh
    修改jdk版本和采集器替换，提供时为配合宝塔，将采集器移到/www/spider目录

--- installbt.sh
+++ installbt.sh
@@ -8,18 +8,23 @@ unzip master.zip
 \cp  yidu-master/conf/server.xml  YiDuInstaller-Nginx/conf/
 \cp  yidu-master/ROOT/WEB-INF/classes/log4j.properties    YiDuInstaller-Nginx/ROOT/WEB-INF/classes/
 cd YiDuInstaller-Nginx
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/otn-pub/java/jdk/8u191-b12/2787e4a523244c269598db4e85c51e0c/jdk-8u191-linux-x64.rpm
+rm -rf  spider
+wget -c https://www.51yd.org/spider20181129.zip
+unzip  spider20181129.zip
 
 
 #安装JDK
-chmod +x jdk-6u45-linux-x64-rpm.bin
-./jdk-6u45-linux-x64-rpm.bin
+chmod +x jdk-8u191-linux-x64.rpm
+rpm -ivh jdk-8u191-linux-x64.rpm
+
 
 #安装TOMCAT
 tar zxvf apache-tomcat-7.0.55.tar.gz
 mv apache-tomcat-7.0.55 /usr/local/tomcat
-\cp conf/server.xml /usr/local/tomcat/conf/   #文件有修改
-\cp sh/tomcat.sh /etc/profile.d/tomcat.sh
-\cp sh/tomcat /etc/rc.d/init.d/tomcat
+\cp -rpf  conf/server.xml /usr/local/tomcat/conf/   #文件有修改
+\cp -rpf  sh/tomcat.sh /etc/profile.d/tomcat.sh
+\cp -rpf sh/tomcat /etc/rc.d/init.d/tomcat
 chmod +x /etc/rc.d/init.d/tomcat
 
 #设置自动启动
@@ -38,7 +43,7 @@ service postgresql-9.3 initdb
 
 #设置开机自动启动
 chkconfig postgresql-9.3 on
-\cp conf/pg_hba.conf /var/lib/pgsql/9.3/data/pg_hba.conf   #需要覆盖命令
+\cp -rpf  conf/pg_hba.conf /var/lib/pgsql/9.3/data/pg_hba.conf   #需要覆盖命令
 
 #启动Postgresql
 service postgresql-9.3 start
@@ -48,11 +53,10 @@ service tomcat start
 
 
 #install spider
-mv spider /usr/local/
-cp sh/spider /etc/rc.d/init.d/spider
+mv spider /www/
+\cp -rpf sh/spider /etc/rc.d/init.d/spider
 chmod +x /etc/rc.d/init.d/spider
 chmod +x /usr/local/spider/start.sh
 chmod +x /usr/local/spider/stop.sh
 chkconfig --add spider
 chkconfig spider on
-
